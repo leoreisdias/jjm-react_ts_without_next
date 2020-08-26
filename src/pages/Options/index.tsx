a7e7a17261fb6d5d91c4a05b8d40d4cd4a889a81
leoreisdias <leoanrdord99@gmail.com>
2020-08-26 00:38:37 -0300

    css responsive

--- src/pages/Options/index.tsx
+++ src/pages/Options/index.tsx
@@ -31,60 +31,47 @@ function Options() {
                 title={`Olá, ${localStorage.getItem('name')}`}
                 backLink="/dashboard"
             >
-                <form id="search-popnewsdetail" >
-                    <div
-                        className="openSidebar">
-                        <p></p>
-                        <p></p>
-                        <FaBars color="white" size={50} onClick={handleSidebar} />
-                    </div>
-                    <p></p>
+                <FaBars className="openSidebar" color="white" size={50} onClick={handleSidebar} />
 
-                </form>
             </PageHeader>
 
 
 
             <MyMenu flag={visible}>
-                <div className="optionsMain">
-                    <div className="sidebarClick" onMouseOver={handleSidebar}>
+                <main className="optionsMain">
+                    <div className="optionsGrid">
+                        <Link to="/news-post">
+                            <div className="optionNews">
+                                <FcNews size={30} />
+                                <h1>Notícias</h1>
+                            </div>
+                        </Link>
+
+                        <Link to="/popnews-post">
+                            <div className="optionPopNews">
+                                <GiGamepad size={35} color="black" />
+                                <h1>Mundo POP</h1>
+                            </div>
+                        </Link>
+
+                        <Link to="/sports-post">
+                            <div className="optionSport">
+                                <GiSoccerBall size={30} color="#E2F0F9" />
+                                <h1>Esportes</h1>
+                            </div>
+                        </Link>
+
+                        <Link to="/report-post">
+                            <div className="optionReport">
+                                <GiDeadWood size={30} color="black" />
+                                <h1>Notas de Falecimento</h1>
+                            </div>
+                        </Link>
 
                     </div>
-                    <main>
-                        <div className="optionsGrid">
-                            <Link to="/news-post">
-                                <div className="optionNews">
-                                    <FcNews size={30} />
-                                    <h1>Notícias</h1>
-                                </div>
-                            </Link>
 
-                            <Link to="/popnews-post">
-                                <div className="optionPopNews">
-                                    <GiGamepad size={35} color="black" />
-                                    <h1>Mundo POP</h1>
-                                </div>
-                            </Link>
+                </main>
 
-                            <Link to="/sports-post">
-                                <div className="optionSport">
-                                    <GiSoccerBall size={30} color="#E2F0F9" />
-                                    <h1>Esportes</h1>
-                                </div>
-                            </Link>
-
-                            <Link to="/report-post">
-                                <div className="optionReport">
-                                    <GiDeadWood size={30} color="black" />
-                                    <h1>Notas de Falecimento</h1>
-                                </div>
-                            </Link>
-
-                        </div>
-
-                    </main>
-
-                </div>
 
 
             </MyMenu>
